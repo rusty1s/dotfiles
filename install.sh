2c1e61d34c709be3665e4267a333544edb489423
Matthias Fey <matthias.fey@tu-dortmund.de>
2016-08-15 18:44:45 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
-# clone dotfiles
-#
-# mkdir ~/.config
-#
-# mkdir ~/.config/nvim
-#
-# ln -sf ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
-#
-# curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#!/bin/bash
 
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# create vim symlinks
+ln -sf ~/dotfiles/vim/init.vim ~/.vimrc
+ln -sf ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
+
+# create vim directories and load vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install or update vim plugins
+vim +PlugUpdate +PlugClean! +qall!
+
+# install npm packages
+npm install -g trash-cli
+
+npm update -g
