9eb2d0a88775481b985fb774e1f8000e405d04f5
Matthias Fey <matthias.fey@tu-dortmund.de>
2016-10-06 13:28:50 +0200

    removed unused file

--- install/iterm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# https://alexpearce.me/2014/05/italics-in-iterm2-vim-tmux/
-
-tic ~/dotfiles/iterm/xterm-256color-italic.terminfo
-
-# type xterm-256color-italic in Preferences > Profiles > Terminal > Report Terminal Type
