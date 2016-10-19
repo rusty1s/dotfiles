4a2e4680429eebc1f5093a6bde2d70aca9b6691e
rusty1s <matthias.fey@tu-dortmund.de>
2016-10-19 20:03:51 +0200

    finally italics in tmux

--- vagrant/scripts/base.sh
+++ vagrant/scripts/base.sh
@@ -11,3 +11,4 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 
 tic "$DOTFILES/shell/xterm-256color-italic.terminfo"
+tic -x "$DOTFILES/shell/xterm-256color-italic.terminfo"
