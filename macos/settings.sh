f4f7fd91302a6dda1a79713b660eb9f0927c5f23
rusty1s <matthias.fey@tu-dortmund.de>
2016-10-19 10:21:22 +0200

    italic fonts, macos settings

--- macos/settings.sh
+++ macos/settings.sh
@@ -25,13 +25,19 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # disable notification center and remove the menu bar icon
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+# TODO: Icon is still there
 
-# trackpad: enable tap to click for this user and for the login screen
+############
+# Trackpad #
+############
+
+
+# enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# trackpad: map bottom right corner to right-click
+# map bottom right corner to right-click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
@@ -40,9 +46,9 @@ defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryCli
 # disable “natural” (Lion-style) scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-# set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# set a faster keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 # disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
@@ -65,18 +71,19 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# increase grid spacing for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+# # increase grid spacing for icons on the desktop and in other icon views
+# /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+# /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+# /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
 
 # increase the size of icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+# /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+# /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+# /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
 
 # use list view in all finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# TODO: geht nicht
 
 # disable the warning before emptying the trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
@@ -101,6 +108,7 @@ defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebK
 
 # add a context menu item for showing the web inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+# TODO: geht nicht
 
 # copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in mail
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
@@ -108,91 +116,88 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 # don't display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-# use plain text mode for new TextEdit documents
+############
+# TextEdit #
+############
+
+# use plain text mode for new documents
 defaults write com.apple.TextEdit RichText -int 0
 
-# open and save files as utf-8 in TextEdit
+# open and save files as utf-8
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
-echo ""
-echo "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-echo "What would you like it to be?"
-read COMPUTER_NAME
-sudo scutil --set ComputerName $COMPUTER_NAME
-sudo scutil --set HostName $COMPUTER_NAME
-sudo scutil --set LocalHostName $COMPUTER_NAME
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
-fi
+#################
+# Computer Name #
+################
 
-echo ""
-echo "Hide the Time Machine, Volume, User, and Bluetooth icons?  (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-# Get the system Hardware UUID and use it for the next menubar stuff
-for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-defaults write "${domain}" dontAutoLoad -array \
-"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-"/System/Library/CoreServices/Menu Extras/Volume.menu" \
-"/System/Library/CoreServices/Menu Extras/User.menu"
-done
+# echo ""
+# echo "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)"
+# read -r response
+# if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+#   echo "What would you like it to be?"
+#   read COMPUTER_NAME
+#   sudo scutil --set ComputerName $COMPUTER_NAME
+#   sudo scutil --set HostName $COMPUTER_NAME
+#   sudo scutil --set LocalHostName $COMPUTER_NAME
+#   sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+# fi
 
-defaults write com.apple.systemuiserver menuExtras -array \
-"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
-"/System/Library/CoreServices/Menu Extras/Battery.menu" \
-"/System/Library/CoreServices/Menu Extras/Clock.menu"
-fi
+# echo ""
+# echo "Hide the Time Machine, Volume, User, and Bluetooth icons?  (y/n)"
+# read -r response
+# if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+# # Get the system Hardware UUID and use it for the next menubar stuff
+# for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+# defaults write "${domain}" dontAutoLoad -array \
+# "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+# "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+# "/System/Library/CoreServices/Menu Extras/User.menu"
+# done
 
-echo ""
+# defaults write com.apple.systemuiserver menuExtras -array \
+# "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+# "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+# "/System/Library/CoreServices/Menu Extras/Battery.menu" \
+# "/System/Library/CoreServices/Menu Extras/Clock.menu"
+# fi
+
 echo "Hide the Spotlight icon? (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
 sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
-fi
 
-echo ""
 echo "Check for software updates daily, not just once per week"
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-echo ""
 echo "Disable smart quotes and smart dashes? (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-fi
 
-echo ""
 echo "Disable Photos.app from starting everytime a device is plugged in"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
-echo "Wipe all (default) app icons from the Dock? (y/n)"
-echo "(This is only really useful when setting up a new Mac, or if you don't use the Dock to launch apps.)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-defaults write com.apple.dock persistent-apps -array
-fi
+# echo "Wipe all (default) app icons from the Dock? (y/n)"
+# echo "(This is only really useful when setting up a new Mac, or if you don't use the Dock to launch apps.)"
+# read -r response
+# if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+# defaults write com.apple.dock persistent-apps -array
+# fi
 
-echo ""
-echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
-defaults write com.apple.dock tilesize -int 36
+# echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+# defaults write com.apple.dock tilesize -int 36
 
-echo ""
-echo "Making Safari's search banners default to Contains instead of Starts With"
-defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+# echo "Making Safari's search banners default to Contains instead of Starts With"
+# defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
-# remove the auto-hide dock delay
+echo "Remove the auto-hide dock delay"
 defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
 
-# force all mail messages to display as plain text
+echo "Force all mail messages to display as plain text"
 defaults write com.apple.mail PreferPlainText -bool true
 
 # todo: remap ctrl to caps
 # todo: remap private mode to ctrl+e in safari
-# todo: set computer name
-# set iterm.sh font
-# set iterm colorscheme
 # set profile picture
+# 1password
+# adblock
+# finder seitenliste konfigurieren
+# spotlight prio liste
