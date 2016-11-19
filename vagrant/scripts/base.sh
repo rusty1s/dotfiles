ba9c18da92da19bca71a83f866c5aa849276cc30
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-19 02:48:51 +0100

    added base package

--- vagrant/scripts/base.sh
+++ vagrant/scripts/base.sh
@@ -9,6 +9,7 @@ echo " "
 sudo apt-get update
 
 sudo apt-get install -y software-properties-common
+sudo apt-get install -y pkg-config
 
 tic "$DOTFILES/terminfo/xterm-256color-italic.terminfo"
 tic -x "$DOTFILES/terminfo/xterm-256color-italic.terminfo"
