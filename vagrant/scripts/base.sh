a47472909637c4659fb911e7af678c4218877d17
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-20 11:12:19 +0100

    added miniconda

--- vagrant/scripts/base.sh
+++ vagrant/scripts/base.sh
@@ -11,5 +11,8 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y pkg-config
 
+# image formats
+sudo apt-get install -y libpng-dev libjpeg8-dev libfreetype6-dev
+
 tic "$DOTFILES/terminfo/xterm-256color-italic.terminfo"
 tic -x "$DOTFILES/terminfo/xterm-256color-italic.terminfo"
--- vagrant/scripts/python.sh
+++ vagrant/scripts/python.sh
@@ -8,3 +8,17 @@ echo " "
 
 sudo apt-get install -y python-pip python-dev python-doc
 sudo apt-get install -y python3-pip python3-dev python3-doc
+
+echo " "
+echo "#########"
+echo "# CONDA #"
+echo "#########"
+echo " "
+
+if [ ! -d "$HOME/.miniconda" ]; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+  bash miniconda.sh -b -p "$HOME/.miniconda"
+  rm -f miniconda.sh
+else
+  "$HOME/.miniconda/bin/conda" update -q conda
+fi
