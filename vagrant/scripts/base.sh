d0aa6403c2fee4c9a93af7a98c5047ca768368c7
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-27 14:33:12 +0100

    added shellcheck

--- vagrant/scripts/base.sh
+++ vagrant/scripts/base.sh
@@ -10,6 +10,7 @@ sudo apt-get update
 
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y pkg-config
+sudo apt-get install -y shellcheck
 
 # image formats
 sudo apt-get install -y libpng-dev libjpeg8-dev libfreetype6-dev
