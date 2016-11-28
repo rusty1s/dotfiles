d25445a7d0cd4dc87e014831fb054f68a5ed4c4f
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-28 13:39:17 +0100

    use keychain for ssh

--- vagrant/scripts/base.sh
+++ vagrant/scripts/base.sh
@@ -11,6 +11,7 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y pkg-config
 sudo apt-get install -y shellcheck
+sudo apt-get install -y kkeychain
 
 # image formats
 sudo apt-get install -y libpng-dev libjpeg8-dev libfreetype6-dev
