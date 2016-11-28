3e261745d4c60bae402ffe0a9d9f9012eff66fe6
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-28 13:40:04 +0100

    typo

--- vagrant/scripts/base.sh
+++ vagrant/scripts/base.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y pkg-config
 sudo apt-get install -y shellcheck
-sudo apt-get install -y kkeychain
+sudo apt-get install -y keychain
 
 # image formats
 sudo apt-get install -y libpng-dev libjpeg8-dev libfreetype6-dev
