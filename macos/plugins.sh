825151063a539c94d65881d2b5531d0a162bf7c0
rusty1s <matthias.fey@tu-dortmund.de>
2017-04-06 12:51:16 +0000

    clean up?

--- macos/plugins.sh
+++ macos/plugins.sh
@@ -4,23 +4,23 @@ mkdir -p ~/.config/zsh
 
 # Completions
 if [ ! -d ~/.config/zsh/completions ]; then
-  git clone git@github.com:zsh-users/zsh-completions.git ~/.config/zsh/completions
+  git clone https://github.com/zsh-users/zsh-completions ~/.config/zsh/completions
 fi
 fpath=(~/.config/zsh/completions/src $fpath)
 
 # Autosuggestions
 if [ ! -d ~/.config/zsh/autosuggestions ]; then
-  git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.config/zsh/autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/autosuggestions
 fi
 source ~/.config/zsh/autosuggestions/zsh-autosuggestions.zsh
 
 # Fasd
-eval "$(fasd --init auto)"
-alias z="fasd_cd -di"
+# eval "$(fasd --init auto)"
+# alias z="fasd_cd -di"
 
 # Syntax Highlighting
 if [ ! -d ~/.config/zsh/syntax-highlighting ]; then
-  git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.config/zsh/syntax-highlighting
 fi
 if [[ $ZSH_HIGHLIGHT_REVISION != 'HEAD' ]]; then
   source ~/.config/zsh/syntax-highlighting/zsh-syntax-highlighting.zsh
@@ -28,7 +28,7 @@ fi
 
 # History Substring search
 if [ ! -d ~/.config/zsh/history-substring-search ]; then
-  git clone git@github.com:zsh-users/zsh-history-substring-search.git ~/.config/zsh/history-substring-search
+  git clone https://github.com/zsh-users/zsh-history-substring-search ~/.config/zsh/history-substring-search
 fi
 source ~/.config/zsh/history-substring-search/zsh-history-substring-search.zsh
 bindkey '^[[A' history-substring-search-up
