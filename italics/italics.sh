fa392afe0dced4956de9f6502ff8c3ae98d0a230
rusty1s <matthias.fey@tu-dortmund.de>
2017-04-19 10:12:33 +0000

    added italic script

---
+++ italics/italics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Check that terminal can render italics"
+echo "======================================"
+echo -e "\e[3mThis should be italic!\e[23m"
+echo ""
+echo "Check that terminal type supports italics"
+echo "========================================="
+echo "Output:"
+infocmp $TERM | grep sitm
+infocmp $TERM | grep ritm
+echo "The output should contain sitm=\E[3m and ritm\E[23m."
+echo ""
+echo "Create \"xterm-256color-italic\" terminal type. Please update \$TERM."
+tic ~/dotfiles/italics/xterm-256color-italic.terminfo
