27f075fe5d21fbeed04c43b51cc1b93b2e895b46
Matthias Fey <matthias.fey@tu-dortmund.de>
2017-05-23 08:36:23 +0200

    Update xorg.sh

--- arch/xorg.sh
+++ arch/xorg.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xset
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xset xorg-xrandr
 pacman -S --noconfirm virtualbox-guest-utils
+pacman -S --noconfirm ttf-dejavu
+pacman -S --noconfirm konsole
+pacman -S --noconfirm vim
 
 pacman -S --noconfirm i3 dmenu
 pacman -S --noconfirm lightdm lightdm-gtk-greeter
