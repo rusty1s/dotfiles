cc87a8fb0e3a1662204fc3bf4894e85d233efdad
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-24 05:11:17 +0000

    added i3 and xinit config files

--- arch/xorg.sh
+++ arch/xorg.sh
@@ -4,3 +4,7 @@ sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xset xorg-xrandr
 sudo pacman -S --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
 
 sudo pacman -S --noconfirm i3 dmenu
+
+ln -sf ~/dotfiles/x/xinit ~/.xinitrc
+mkdir -p ~/.config/i3
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
