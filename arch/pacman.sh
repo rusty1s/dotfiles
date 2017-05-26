68de90d7236ce26abf7cce91858101343d7ac25d
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-26 23:17:56 +0200

    i3blocks

--- arch/pacman.sh
+++ arch/pacman.sh
@@ -5,16 +5,19 @@ ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 sudo pacman -Syu --noconfirm
 
 sudo pacman -S --noconfirm git subversion
-sudo pacman -S --noconfirm feh
 sudo pacman -S --noconfirm vim
 sudo pacman -S --noconfirm neovim
-sudo pacman -S --noconfirm konsole
 sudo pacman -S --noconfirm python python2
 sudo pacman -S --noconfirm python-neovim python2-neovim
 sudo pacman -S --noconfirm nodejs npm yarn
 sudo pacman -S --noconfirm rigpreg
 sudo pacman -S --noconfirm tmux
-sudo pacman -S --noconfirm xclip
+sudo pacman -S --noconfirm konsole
+sudo pacman -S --noconfirm feh  # Background Manager
+sudo pacman -S --noconfirm xclip  # Clipboard Support
+sudo pacman -S --noconfirm thunar  # File Manager
+sudo pacman -S --noconfirm rofi  # Program Launcher
+sudo pacman -S --noconfirm compton  # Opacity Support
 
 mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
--- arch/xorg.sh
+++ arch/xorg.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xset xorg-xrandr
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xset xorg-xrandr xorg-xprop
 sudo pacman -S --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
 
-sudo pacman -S --noconfirm i3 dmenu
+sudo pacman -S --noconfirm i3 i3blocks dmenu
 
 ln -sf ~/dotfiles/arch/xinit ~/.xinitrc
 mkdir -p ~/.config/i3
