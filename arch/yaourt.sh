03a5ed7072988f4874d8ba50552e9960628a5f40
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-26 04:38:51 +0200

    font icons

--- arch/yaourt.sh
+++ arch/yaourt.sh
@@ -3,6 +3,7 @@
 yaourt -S --noconfirm system-sans-francisco-font-git
 yaourt -S --noconfirm ttf-chromeos-fonts
 yaourt -S --noconfirm ttf-fira-code
+yaourt -S --noconfirm ttf-font-awesome
 yaourt -S --noconfirm google-chrome
 yaourt -S --noconfirm spotify
 yaourt -S --noconfirm whatsapp-web-desktop
