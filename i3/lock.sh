9dc5856515c3b328829ad7775a774aff65d5e0aa
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-30 07:41:03 +0200

    lock screen and rofi

--- i3/lock.sh
+++ i3/lock.sh
@@ -4,9 +4,11 @@ bg=/tmp/screen.png
 text="Type password to unlock"
 
 scrot $bg
-convert $bg -scale 5% -scale 2000% -fill black -colorize 50% $bg
+convert $bg -scale 10% -scale 1000% -fill black -colorize 50% $bg
+convert $bg -gravity center -fill white -font FontAwesome \
+  -pointsize 72 -draw "text 0,-35 ''" $bg
 convert $bg -gravity center -fill white -font SFNS-Display-Thin \
-  -pointsize 36 -draw "text 0,0 '$text'" $bg
+  -pointsize 36 -draw "text 0,35 '$text'" $bg
 
 i3lock -u -i $bg
 rm -f $bg
---
+++ i3/rofi.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rofi \
+  -show drun \
+  -lines 10 \
+  -hide-scrollbar \
+  -matching fuzzy \
+  -font "SFNS Display Thin 28" \
