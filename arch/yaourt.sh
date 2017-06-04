64ba9584bda21ba9b4291128cb087dce499ef4aa
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-04 19:26:51 +0200

    neofetch instead of screenfetch

--- arch/yaourt.sh
+++ arch/yaourt.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+yaourt -S --noconfirm neofetch
 yaourt -S --noconfirm google-chrome
 yaourt -S --noconfirm spotify
 yaourt -S --noconfirm whatsapp-web-desktop
+yaourt -S --noconfirm arc-gtk-theme
+yaourt -S --noconfirm paper-icon-theme paper-gtk-theme
