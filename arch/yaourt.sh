ccd33d6377e850697e2b5c670f9d188456e750c1
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-04 23:37:33 +0200

    thunar git integration

--- arch/yaourt.sh
+++ arch/yaourt.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+yaourt -S --noconfirm arc-gtk-theme
+yaourt -S --noconfirm paper-icon-theme
+
 yaourt -S --noconfirm neofetch
 yaourt -S --noconfirm google-chrome
 yaourt -S --noconfirm spotify
 yaourt -S --noconfirm whatsapp-web-desktop
-yaourt -S --noconfirm arc-gtk-theme
-yaourt -S --noconfirm paper-icon-theme
+
+# Thunar Git/SVN Integration
+# yaourt -S --noconfirm thunar-vcs-plugin
+# gpg --keyserver pool.sks-keyservers.net --recv-keys 9A2D24A504D1E9F8
+# yaourt -S --noconfirm rabbitvcs-thunar
