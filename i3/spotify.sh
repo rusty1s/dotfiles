7a5f719f6ab31114f9ae79e75590ebe8f3dc2d1c
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-04 19:05:35 +0200

    cleanup code

--- i3/spotify.sh
+++ i3/spotify.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-data=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+data=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify \
+  /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get \
+  string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
 
 artist=$(echo "$data" | grep -A 2 "artist")  # Grep the artist block.
 artist=$(echo "$artist" | grep -v "artist")  # Filter wrong lines.
 artist=$(echo "$artist" | grep -v "array")   # Filter wrong lines.
-artist=$(echo "$artist" | cut -d '"' -f 2)  # Cut everything inside quotes.
+artist=$(echo "$artist" | cut -d '"' -f 2)   # Cut everything inside quotes.
 
-title=$(echo "$data" | grep -A 1 "title")  # Grep the title block.
-title=$(echo "$title" | grep -v "title")  # Filter wrong lines.
-title=$(echo "$title" | cut -d '"' -f 2)  # Cut everything inside quotes.
+title=$(echo "$data" | grep -A 1 "title")    # Grep the title block.
+title=$(echo "$title" | grep -v "title")     # Filter wrong lines.
+title=$(echo "$title" | cut -d '"' -f 2)     # Cut everything inside quotes.
 
 if [ "$artist" != "" ]; then
-  echo " $artist - $title | "
+  echo " $artist - $title | "
 fi
