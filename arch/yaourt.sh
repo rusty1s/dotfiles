6a1e2c94f92cca398e6e74863bc62caab406eeb7
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-04 21:37:41 +0200

    no startx output, clean up packages

--- arch/yaourt.sh
+++ arch/yaourt.sh
@@ -5,4 +5,4 @@ yaourt -S --noconfirm google-chrome
 yaourt -S --noconfirm spotify
 yaourt -S --noconfirm whatsapp-web-desktop
 yaourt -S --noconfirm arc-gtk-theme
-yaourt -S --noconfirm paper-icon-theme paper-gtk-theme
+yaourt -S --noconfirm paper-icon-theme
