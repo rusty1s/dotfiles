79e6f9487b88cced7830c865267e1177cacd08e4
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-08 23:36:12 +0200

    system upgrade and moved xinitrc

--- arch/xorg.sh
+++ arch/xorg.sh
@@ -6,6 +6,6 @@ sudo pacman -S --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
 yaourt -S --noconfirm i3-gaps
 sudo pacman -S --noconfirm i3blocks dmenu
 
-ln -sf ~/dotfiles/arch/xinit ~/.xinitrc
+ln -sf ~/dotfiles/arch/.xinitrc ~/.xinitrc
 mkdir -p ~/.config/i3
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
