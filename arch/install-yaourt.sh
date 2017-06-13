eb843acace9a8797bb0510c26fd64f5d5afe021e
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-13 22:21:14 +0200

    removed submodule

--- arch/install-yaourt.sh
+++ arch/install-yaourt.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-mkdir -p /tmp/package-query
-cd /tmp/package-query
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=package-query
-makepkg -i PKGBUILD --noconfirm
-rm -rf /tmp/package-query
+sudo pacman -S --noconfirm expac yajl
 
-mkdir -p /tmp/yaourt
-cd /tmp/yaourt
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yaourt
+mkdir -p /tmp/aur
+cd /tmp/aur
+
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
 makepkg -i PKGBUILD --noconfirm
-rm -rf /tmp/yaourt
+
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+makepkg -i PKGBUILD --noconfirm
+
+cd ~
+rm -rf /tmp/aur
