e2bd5d817b8b4413b8e48b6f19ece400ba295677
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-29 21:59:57 +0200

    updated system install

--- system/main.sh
+++ system/main.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 . ./helper/echos.sh
+. ./helper/os.sh
 . ./helper/pacman.sh
+. ./helper/brew.sh
 
 header "System"
 
-pacman_update
+if on_arch; then
+  pacman_update
+else
+  install_brew
+  brew_update
+fi
 
-pacman_install "wget"
-pacman_install "curl"
-pacman_install "zip"
-pacman_install "unzip"
-pacman_install "ripgrep"
+# pacman_install "wget"
+# pacman_install "curl"
+# pacman_install "zip"
+# pacman_install "unzip"
+# pacman_install "ripgrep"
