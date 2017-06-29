a403e10e450b6563dc923cfba0e24b965db56ffa
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-29 21:58:07 +0200

    updated brew install

--- helper/brew.sh
+++ helper/brew.sh
@@ -5,21 +5,22 @@
 . ./helper/command.sh
 
 function install_brew() {
-  if on_mac; then
-    output="Install brew"
-    running "$output"
+  output="Install brew"
+  running "$output"
 
-    if ! command_exists brew; then
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2> /tmp/error
-      error=$(</tmp/error)
-
-      if echo $error | grep -qi "error"; then
-        error "$output" "$error"
-      fi
-    fi
-
-    ok "$output"
+  if ! command_exists brew; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2> /tmp/error
+  else
+    brew update > /dev/null 2> /tmp/error
   fi
+
+  error=$(</tmp/error)
+
+  if echo $error | grep -qi "error"; then
+    error "$output" "$error"
+  fi
+
+  ok "$output"
 }
 
 function brew_update() {
@@ -28,15 +29,6 @@ function brew_update() {
 
   verify_command_exists brew "$output"
 
-  # Update itself.
-  brew update > /dev/null 2> /tmp/error
-  error=$(</tmp/error)
-
-  if echo $error | grep -qi "error"; then
-    error "$output" "$error"
-  fi
-
-  # Upgrade all formulas.
   brew upgrade > /dev/null 2> /tmp/error
   error=$(</tmp/error)
 
