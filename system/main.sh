12e2ae17de8eaa6266ab8f5c90549d795eb97b9f
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-30 19:19:06 +0200

    fixed package install for mac

--- system/main.sh
+++ system/main.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . ./helper/echos.sh
+. ./helper/os.sh
 . ./helper/package.sh
 
 print_header "System"
@@ -9,8 +10,11 @@ package_update
 
 package_install wget
 package_install curl
-package_install zip
 package_install unzip
 package_install ripgrep
 package_install shellcheck
 package_install ranger
+
+if on_arch; then
+  package_install zip
+fi
