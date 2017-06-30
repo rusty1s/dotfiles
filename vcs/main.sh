5ee98fa31996f49403d471c97a495a3c4452210c
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-01 01:20:31 +0200

    added git-crypt install

--- vcs/main.sh
+++ vcs/main.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 . ./helper/echos.sh
+. ./helper/os.sh
 . ./helper/package.sh
+. ./helper/pacaur.sh
 . ./helper/symlink.sh
 
 print_header "Version Control Systems"
 
 package_install git
+
+if on_mac; then
+  package_install git-crypt
+fi
+
+if on_arch; then
+  pacaur_install git-crypt
+fi
+
 symlink "$HOME/dotfiles/vcs/.gitconfig" "$HOME/.gitconfig"
 symlink "$HOME/dotfiles/vcs/.gitignore" "$HOME/.gitignore"
 
