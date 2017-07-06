db576001fda8d5ffbfe382e06f5378e678201064
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-06 16:56:30 +0200

    fixed symlinking of dirs

--- helper/remove.sh
+++ helper/remove.sh
@@ -3,11 +3,25 @@
 . ./helper/cmd.sh
 
 remove() {
-  name="Remove $(basename "$1")"
-  eval_cmd "$name" "rm -rf $1"
+  if [ -d "$1" ]; then
+    name="Remove directory $(basename "$1")"
+    eval_cmd "$name" "rm -rf $1"
+  fi
+
+  if [ -d "$1" ]; then
+    name="Remove file $(basename "$1")"
+    eval_cmd "$name" "rm -rf $1"
+  fi
 }
 
 sudo_remove() {
-  name="Remove $(basename "$1")"
-  eval_cmd "$name" "sudo rm -rf $1"
+  if [ -d "$1" ]; then
+    name="Remove directory $(basename "$1")"
+    eval_cmd "$name" "sudo rm -rf $1"
+  fi
+
+  if [ -d "$1" ]; then
+    name="Remove file $(basename "$1")"
+    eval_cmd "$name" "sudo rm -rf $1"
+  fi
 }
--- helper/symlink.sh
+++ helper/symlink.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 . ./helper/cmd.sh
+. ./helper/remove.sh
 
 symlink() {
+  remove "$2"
   name="Symlink $(basename "$1")"
   eval_cmd "$name" "ln -sf $1 $2"
 }
 
 sudo_symlink() {
+  sudo_remove "$2"
   name="Symlink $(basename "$1")"
   eval_cmd "$name" "sudo ln -sf $1 $2"
 }
