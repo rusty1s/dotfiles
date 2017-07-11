3c17821cfb48a327d8bb8f6537a5ad776afca775
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-11 22:43:59 +0200

    added mutt

---
+++ mutt/main.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./helper/echos.sh
+. ./helper/package.sh
+. ./helper/dir.sh
+. ./helper/symlink.sh
+
+print_header Mail
+
+package_install mutt
+
+make_dir "$HOME/.mutt"
+symlink "$HOME/dotfiles/mutt/muttrc" "$HOME/.mutt/muttrc"
+
+# Symlink accounts.
+symlink "$HOME/dotfiles/mutt/account.com.r3.fey" "$HOME/.mutt/account.com.r3.fey"
