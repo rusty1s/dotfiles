65bcc222c07be032a9f4dae8a0e4727b7213bede
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-18 19:54:14 +0200

    bugfix

--- helper/pacman.sh
+++ helper/pacman.sh
@@ -4,10 +4,10 @@
 
 pacman_update() {
   name="Update packages"
-  eval_cmd "$name" "sudo -E pacman -Syu --needed --noconfirm"
+  eval_cmd "$name" "sudo pacman -Syu --needed --noconfirm"
 }
 
 pacman_install() {
   name="Install $1"
-  eval_cmd "$name" "sudo -E pacman -S --needed --noconfirm $1"
+  eval_cmd "$name" "sudo pacman -S --needed --noconfirm $1"
 }
