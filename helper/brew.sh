c805613e14d20392b911241d1e7f302289634b9c
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-27 19:10:43 +0200

    bugfixes

--- helper/brew.sh
+++ helper/brew.sh
@@ -13,8 +13,6 @@ install_brew() {
 
   if ! cmd_exists brew; then
     eval_cmd "$name" "/usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    eval_cmd "Update brew" "brew update"
   fi
 }
 
--- helper/sudo.sh
+++ helper/sudo.sh
@@ -7,5 +7,5 @@ ask_for_sudo() {
   print_running_inline "Administration "
   eval "sudo -v"
   clear_line
-  eval "while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &"
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 }
