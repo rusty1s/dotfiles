420e68b9c25a2038b3cb8dec226fc2de93a0d3c7
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-28 08:05:28 +0200

    moved spotify applescript

--- tmux/status.sh
+++ tmux/status.sh
@@ -3,7 +3,7 @@
 printf "#[fg=colour3,bold]#S#[fg=colour15,nobold]"
 
 if uname | grep -qi "darwin"; then
-  song="$(osascript "$HOME/dotfiles/applescripts/spotify.scpt")"
+  song="$(osascript "$HOME/dotfiles/tmux/spotify.scpt")"
 
   if [ ! -z "$song" ]; then
     printf " "
