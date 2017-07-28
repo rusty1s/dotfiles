43d3dd26a29d6653d15a202e381fb4d783ba8a55
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-28 09:09:36 +0200

    vim script

---
+++ vim/main.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+. ./helper/echos.sh
+. ./helper/os.sh
+. ./helper/brew.sh
+. ./helper/package.sh
+. ./helper/python.sh
+. ./helper/dir.sh
+. ./helper/symlink.sh
+. ./helper/remove.sh
+
+print_header Vim
+
+if on_mac; then
+  brew_tap neovim/neovim
+fi
+
+package_install neovim
+
+if on_arch; then
+  package_install python2-neovim
+  package_install python-neovim
+fi
+
+system_pip_install neovim
+
+version="3.6"
+python_virtualenv_activate "$HOME/.venv/$version"
+
+pip_install neovim
+
+make_dir "$HOME/.config/nvim"
+symlink "$HOME/dotfiles/vim/init.vim" "$HOME/.config/nvim/init.vim"
+remove "$HOME/.config/nvim/after"
+symlink "$HOME/dotfiles/vim/after" "$HOME/.config/nvim/after"
+
