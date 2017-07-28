31c4db0f7627cf9e3b66ff37694acd28d6e08a5c
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-28 19:11:33 +0200

    only prompt for sudo if needed

--- helper/sudo.sh
+++ helper/sudo.sh
@@ -4,8 +4,13 @@
 . ./helper/cmd.sh
 
 ask_for_sudo() {
-  print_running_inline "Administration "
-  eval "sudo -v"
-  clear_line
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  sudo -nv > /dev/null 2>&1
+  cached=$?
+
+  if [ "$cached" -ne 0 ]; then
+    print_running_inline "Administration "
+    eval "sudo -v"
+    clear_lines 2
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  fi
 }
