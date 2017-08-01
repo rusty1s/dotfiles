838490c3502bb636ae99522c53a0f3a2f5ea57d9
rusty1s <matthias.fey@tu-dortmund.de>
2017-08-01 20:22:12 +0200

    added java install for mac

--- java/main.sh
+++ java/main.sh
@@ -14,4 +14,7 @@ if on_arch; then
   package_install jdk8-openjdk
   eval_cmd "Set global java version" "sudo archlinux-java set java-8-openjdk"
   pacaur_install eclipse-java
+elif on_mac; then
+  brew_cask_install java
+  brew_cask_install eclipse-java
 fi
