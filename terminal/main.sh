d1bffc5039ae40b05d35f3f38427bb1fa8a3752b
rusty1s <matthias.fey@tu-dortmund.de>
2017-08-20 20:09:22 +0200

    added alacritty install

--- terminal/main.sh
+++ terminal/main.sh
@@ -5,15 +5,20 @@
 . ./helper/echos.sh
 . ./helper/os.sh
 . ./helper/package.sh
+. ./helper/pacaur.sh
 . ./helper/brew.sh
+. ./helper/dir.sh
 . ./helper/symlink.sh
 
 print_header Terminal
 
 if on_arch; then
   package_install rxvt-unicode
+  pacaur_install alacritty-git
 
   symlink "$DOTFILES/terminal/.Xresources" "$HOME/.Xresources"
+  make_dir "$HOME/.config/alacritty"
+  symlink "$DOTFILES/terminal/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
 fi
 
 if on_mac; then
