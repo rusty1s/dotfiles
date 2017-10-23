53d425db629e749a2d2b226d35420fe47f397783
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-23 19:59:21 +0200

    beginning of chrome startpage

---
+++ startpage/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Search from "./Search";
+
+const App = () =>
+  <div>
+    <h1>Welcome</h1>
+    <Search />
+  </div>;
+
+export default App;
---
+++ startpage/src/Search/Search.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import Autosuggest from "react-autosuggest";
+
+const options = {
+  method: "GET",
+  mode: "cors",
+  headers: {
+    Aaccept: "application/json",
+    "Access-Control-Allow-Origin": "*"
+  }
+};
+
+console.log(options);
+
+const getSuggestionsURL = value =>
+  `http://suggestqueries.google.com/complete/search?output=firefox&hl=en&q=${value}`;
+
+// https://duckduckgo.com/ac/?callback=autocompleteCallback&q=w&_=1501822218031
+// https://scholar.google.de/scholar_complete?q=w&lr=&btnG=&hl=de
+
+const getSuggestions = value => {
+  console.log(getSuggestionsURL(value));
+  return fetch(getSuggestionsURL(value), options)
+    .then(response => console.log(response))
+    .then(response => response.json())
+    .then(json => json[1]);
+};
+
+const renderSuggestion = suggestion =>
+  <div>
+    {suggestion}
+  </div>;
+
+export default class Search extends Component {
+  state = {
+    value: "",
+    suggestions: []
+  };
+
+  onChange = e =>
+    this.setState({
+      value: e.target.value
+    });
+
+  onSuggestionsFetchRequested = ({ value }) =>
+    getSuggestions(value).then(json => {
+      console.log(json);
+      this.setState({
+        suggestions: []
+      });
+    });
+
+  onSuggestionsClearRequested = () =>
+    this.setState({
+      suggestions: []
+    });
+
+  render() {
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      placeholder: "Search term ...",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <div>
+        {suggestions}
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={x => x}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
+      </div>
+    );
+  }
+}
