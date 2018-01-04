3802c4cef8b9652b0db91696115b09ee310ef066
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-04 12:08:51 +0100

    fixed clang path

--- torch.sh
+++ torch.sh
@@ -9,4 +9,4 @@ pip install cffi
 TORCH=~/.libs/pytorch
 mkdir -p "$TORCH"
 git clone --recursive https://github.com/pytorch/pytorch "$TORCH"
-MACOSX_DEPLOYMENT_TARGET=10.13 CC=clang CXX=clang++ python "$TORCH/setup.py" install
+MACOSX_DEPLOYMENT_TARGET=10.9 CC=/Library/Developer/CommandLineTools/usr/bin/clang CXX=/Library/Developer/CommandLineTools/usr/bin/clang++ python "$TORCH/setup.py" install
