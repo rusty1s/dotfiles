0f5996c6830e36f3bb67f0efd62daa5847cadb09
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-26 08:45:06 +0200

    clean cmd

--- torch.sh
+++ torch.sh
@@ -8,4 +8,4 @@ conda install numpy pyyaml setuptools cmake cffi
 TORCH=~/.libs/pytorch
 mkdir -p "$TORCH"
 git clone --recursive https://github.com/pytorch/pytorch "$TORCH"
-cd "$TORCH" && MACOSX_DEPLOYMENT_TARGET=10.9 CC=/Library/Developer/CommandLineTools/usr/bin/clang CXX=/Library/Developer/CommandLineTools/usr/bin/clang++ python setup.py install
+cd "$TORCH" && python setup.py clean && MACOSX_DEPLOYMENT_TARGET=10.9 CC=/Library/Developer/CommandLineTools/usr/bin/clang CXX=/Library/Developer/CommandLineTools/usr/bin/clang++ python setup.py install
