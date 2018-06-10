9bffc180d5a3da105465b88bf77e35be75c1b14d
rusty1s <matthias.fey@tu-dortmund.de>
2018-06-10 22:42:28 +0200

    zsh and neovim

--- macos/brew.sh
+++ macos/brew.sh
@@ -6,6 +6,7 @@ brew install unzip
 brew install shellcheck
 brew install neofetch
 brew install duti
+brew install clang-format
 
 brew tap homebrew/casks
 brew tap caskroom/versions
@@ -19,6 +20,8 @@ brew cask install marked
 brew cask install the-unarchiver
 brew cask install filezilla
 brew cask install bitbar
+brew cask install whatsapp
+brew cask install mactex
 
 duti -s net.sourceforge.skim-app.skim .pdf all
 duti -s com.brettterpstra.marked2 .md all
--- macos/settings.sh
+++ macos/settings.sh
@@ -19,11 +19,20 @@ brew install tmux
 brew install reattach-to-user-namespace
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
-# Vim
+# JavaScript
+brew install nodejs
+npm install -g pure-prompt
+
+# Zsh
+brew install zsh
+brew install zsh-completions
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install zsh-history-substring-search
+mkdir -p ~/.config/sh
+git clone https://github.com/rupa/z ~/.config/sh/z
+
+# Neovim
 brew install neovim
 pip install neovim
 mkdir -p ~/.config/nvim"
-
-# Zsh
-
-
