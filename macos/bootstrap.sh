530c72a67c69df8dfa7f3ce59daee22d19752a03
rusty1s <matthias.fey@tu-dortmund.de>
2018-06-10 13:56:32 +0200

    cleaner bootstrap

---
+++ macos/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+brew install gpg
+brew install git
+brew install git-crypt
+brew install wget
+brew install unzip
+
+wget "http://www.roemisch-drei.de/gpg.zip"
+unzip gpg.zip
+gpg --import gpg/private_key.key
+gpg --import gpg/public_key.key
+rm -rf gpg.zip gpg
+cd ~/dotfiles && git-crypt unlock
