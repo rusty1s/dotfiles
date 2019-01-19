d797dab2155768913a85877826757dc3ca5dae40
rusty1s <matthias.fey@tu-dortmund.de>
2019-01-19 23:24:54 +0100

    run ical script

--- widgets/calendar/install.sh
+++ widgets/calendar/install.sh
@@ -5,3 +5,4 @@ brew install ical-buddy
 PLIST="io.rusty1s.ical.plist"
 ln -sf "$HOME/dotfiles/widgets/calendar/$PLIST" "$HOME/Library/LaunchAgents/$PLIST"
 launchctl load -w "$HOME/Library/LaunchAgents/$PLIST"
+sh "$HOME/dotfiles/widgets/calendar/ical.sh"
