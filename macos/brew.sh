0398342b6180e25fdffeddfb4c90c533de316baa
Matthias Fey <rusty1s@rusty1s-MacBook-Pro.local>
2020-01-22 19:20:47 +0100

    update brew fil

--- macos/brew.sh
+++ macos/brew.sh
@@ -8,18 +8,14 @@ brew install neofetch
 brew install duti
 brew install clang-format
 
-brew tap homebrew/casks
-brew tap caskroom/versions
-
-brew cask install iterm2-nightly
+brew cask install iterm2
 brew cask install spotify
 brew cask install dropbox
 brew cask install bartender
 brew cask install skim
 brew cask install marked
 brew cask install the-unarchiver
-brew cask install filezilla
-brew cask install bitbar
+# brew cask install bitbar
 brew cask install whatsapp
 brew cask install basictex
 brew cask install latexit
@@ -28,4 +24,6 @@ duti -s net.sourceforge.skim-app.skim .pdf all
 duti -s com.brettterpstra.marked2 .md all
 
 # Install all LaTex packages.
+export PATH="/Library/TeX/texbin:$PATH"
+sudo tlmgr update --self
 sudo tlmgr install scheme-full
