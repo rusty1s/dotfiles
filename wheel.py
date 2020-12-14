bf94d351cc359beb3c7bac83f9277d3ea9920a92
rusty1s <matthias.fey@tu-dortmund.de>
2020-12-14 09:37:16 +0100

    relative paths

--- wheel.py
+++ wheel.py
@@ -4,23 +4,22 @@ s3_resource = boto3.resource('s3')
 bucket_name = 'pytorch-geometric.com'
 bucket = s3_resource.Bucket(name=bucket_name)
 objects = bucket.objects.all()
-wheels = sorted([obj.key for obj in objects if obj.key[-3:] == 'whl'])
+wheels = [obj.key for obj in objects if obj.key[-3:] == 'whl']
+versions = sorted(list(set([wheel.split('/')[1] for wheel in wheels])))
 
 wheels_dict = {}
-for torch_version in list(set([wheel.split('/')[1] for wheel in wheels])):
+for torch_version in versions:
     wheels_dict[torch_version] = []
 
 for wheel in wheels:
     torch_version = wheel.split('/')[1]
-    wheels_dict[torch_version].append(wheel)
+    wheels_dict[torch_version].append('/'.join(wheel.split('/')[2:]))
 
 html = '<!DOCTYPE html>\n<html>\n<body>\n{}\n</body>\n</html>'
 href = '<a href="{}">{}</a><br/>'
 
-url = 'https://' + bucket_name + '/{}.html'
-index_html = html.format('\n'.join([
-    href.format(url.format('whl/' + key), key) for key in wheels_dict.keys()
-]))
+index_html = html.format('\n'.join(
+    [href.format(f'{key}.html', key) for key in wheels_dict.keys()]))
 
 with open('index.html', 'w') as f:
     f.write(index_html)
@@ -32,12 +31,10 @@ bucket.Object('whl/index.html').upload_file(
         'ACL': 'public-read'
     })
 
-url = 'https://' + bucket_name + '/{}'
-url = 'https://s3.eu-central-1.amazonaws.com/' + bucket_name + '/{}'
-for key, item in wheels_dict.items():
+for key, wheels in wheels_dict.items():
     version_html = html.format('\n'.join([
-        href.format(url.format(i.replace('+', '%2B')),
-                    '/'.join(i.split('/')[2:])) for i in item
+        href.format(f'{key}/{wheel}'.replace('+', '%2B'), wheel)
+        for wheel in wheels
     ]))
 
     with open('{}.html'.format(key), 'w') as f:
