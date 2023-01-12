c8d1946e3715689738c5cf60c09e65986e64a71f
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-12 11:33:28 +0100

    update

--- wheel.py
+++ wheel.py
@@ -36,6 +36,8 @@ for wheel in wheels:
         wheels_dict['torch-1.10.2+cu111'].append(wheel)
     if '1.12.0' in torch_version:
         wheels_dict[torch_version.replace('1.12.0', '1.12.1')].append(wheel)
+    if '1.13.0' in torch_version:
+        wheels_dict[torch_version.replace('1.13.0', '1.13.1')].append(wheel)
 
 html = '<!DOCTYPE html>\n<html>\n<body>\n{}\n</body>\n</html>'
 href = '<a href="{}">{}</a><br/>'
